295c5d5f5853d1f2d24ca385b478b574d6653439
Beyza Nil Uysal <beyzanil.313@gmail.com>
2023-07-21 13:13:26 +0300

    Ödev2: Sadece istenen dosyalar

---
+++ GenericRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+public interface IGenericRepository<TEntity> where TEntity : class
+{
+    IEnumerable<TEntity> GetByParameter(Expression<Func<TEntity, bool>> filter);
+}
+
+public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+{
+    private readonly DbContext _context;
+
+    public GenericRepository(DbContext context)
+    {
+        _context = context;
+    }
+
+    public IEnumerable<TEntity> GetByParameter(Expression<Func<TEntity, bool>> filter)
+    {
+        return _context.Set<TEntity>().Where(filter).ToList();
+    }
+}
+
---
+++ TransactionController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TransactionController : ControllerBase
+{
+    private readonly IGenericRepository<Transaction> _transactionRepository;
+
+    public TransactionController(IGenericRepository<Transaction> transactionRepository)
+    {
+        _transactionRepository = transactionRepository;
+    }
+
+    [HttpGet]
+    public IActionResult GetByParameter(
+        string accountNumber,
+        string referenceNumber,
+        decimal? minAmountCredit,
+        decimal? maxAmountCredit,
+        decimal? minAmountDebit,
+        decimal? maxAmountDebit,
+        string description,
+        DateTime? beginDate,
+        DateTime? endDate)
+    {
+        // Filtre kriterlerine göre Lambda ifadesi oluşturma
+        Expression<Func<Transaction, bool>> filter = transaction =>
+            (string.IsNullOrEmpty(accountNumber) || transaction.AccountNumber == accountNumber) &&
+            (string.IsNullOrEmpty(referenceNumber) || transaction.ReferenceNumber == referenceNumber) &&
+            (!minAmountCredit.HasValue || transaction.AmountCredit >= minAmountCredit.Value) &&
+            (!maxAmountCredit.HasValue || transaction.AmountCredit <= maxAmountCredit.Value) &&
+            (!minAmountDebit.HasValue || transaction.AmountDebit >= minAmountDebit.Value) &&
+            (!maxAmountDebit.HasValue || transaction.AmountDebit <= maxAmountDebit.Value) &&
+            (string.IsNullOrEmpty(description) || transaction.Description == description) &&
+            (!beginDate.HasValue || transaction.Date >= beginDate.Value) &&
+            (!endDate.HasValue || transaction.Date <= endDate.Value);
+
+        var transactions = _transactionRepository.GetByParameter(filter);
+        return Ok(transactions);
+    }
+}
+
